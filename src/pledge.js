6c5c80448fb42e6fea18b8c6c8b0574c11bbe367
Bojan Jovanovic <bojanj6m@gmail.com>
2017-08-09 12:09:16 -0400

    reject and resolve dont overwirte each other

--- src/pledge.js
+++ src/pledge.js
@@ -4,9 +4,33 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise(executor){
+    if(typeof executor !== 'function') {
+        throw new TypeError(/executor.+function/i);
+    }
+    this._state = 'pending';
+    //this.constructor = new Promise(executor);
+    this._internalResolve = function(obj){
+        if (this._state === 'fulfilled' || this._state === 'rejected'){
+          return;  
+        }
+        this._value = obj;
+        this._state = 'fulfilled';
 
+    }
+    this._internalReject = function(reason){
+        if (this._state === 'rejected' || this._state === 'fulfilled') {
+            return;
+        }
+        this._state = 'rejected';
+        this._value = reason;
+    }
+}
 
+$Promise.prototype.constructor = new Promise(executor);
+// $Promise.prototype._internalResolve = function() {
 
+// }
 
 
 /*-------------------------------------------------------
